696c452ffdbdc0a7f136ca503dc63eb6dd641e50
Carlos Botina <carlosalberto.botinacarpio@gmail.com>
2024-07-06 11:42:37 -0500

    Setup ConfigModule & TypeORM Module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,27 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { environments } from 'config/environments';
+import configuration from 'config/conriguration';
+import configValidation from 'config/validation/config-validation.schema';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { dbConfig } from 'config/database/database.config';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      envFilePath: environments[process.env.NODE_ENV],
+      load: [configuration],
+      validationSchema: configValidation,
+      isGlobal: true,
+    }),
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      useFactory: dbConfig,
+      inject: [ConfigService],
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
